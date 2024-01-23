22064bd3d1118ffa42b4f56e4a3d97944a93b06e
pratikp_72 <pratikp72@hotmail.com>
2024-01-23 23:31:12 +0530

    update the folder tree logic

--- client/app/component/files/folder.tree.tsx
+++ client/app/component/files/folder.tree.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { memo, useCallback, useEffect, useState } from 'react';
 import { TreeView } from '@mui/x-tree-view/TreeView';
 import { TreeItem } from '@mui/x-tree-view/TreeItem';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { Box, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Typography } from '@mui/material';
-import { Folder, MoreVert, CloudUpload, CreateNewFolder } from '@mui/icons-material';
+import { Box, Grid, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, TextField, Typography, colors } from '@mui/material';
+import { Folder, MoreVert, CloudUpload, CreateNewFolder, Check, Close } from '@mui/icons-material';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
+const AddFolderSchema = Yup.object().shape({
+    folderName: Yup.string().required('Required')
+ });
 
 interface BoxWithIconProps{
     label: string;
+    id: number;
+    toggleAddFolder: CallableFunction;
 }
 
 interface DotPopoverProps{
-    ids: string;
+    treeId: number;
+    toggleAddFolder: CallableFunction;
+}
+
+interface AddNewFolderProps{
+    toggleAddFolder: CallableFunction;
 }
 
 interface FolderMetaData{
@@ -22,32 +34,41 @@ interface FolderMetaData{
 }
 interface TreeListingProps{
     folderData: Array<FolderMetaData>;
+    folderHook: CallableFunction;
+    newflag: boolean;
+    andChangeFlag: CallableFunction;
 }
 
 export const FolderTree = () => {
    
-    const BoxWithIcon = ({label}:BoxWithIconProps) => {
+    const BoxWithIcon = ({label, id, toggleAddFolder}:BoxWithIconProps) => {
         return (
-            <Box component="div" sx={{display: 'flex', p: 1}}>
+            <Box component="div" sx={{display: 'flex', p: 1, color: colors.grey[700]}}>
                 <Box component={Folder} ></Box>
                 <Box component="div" sx={{display: "flex", justifyContent: "space-between", flex:1}}>
                     <Typography component="span" sx={{ml: 1/2, mr: 1/2}}>{label}</Typography>
-                    <DotPopoverMenu ids="ss"></DotPopoverMenu>
+                    <DotPopoverMenu treeId={id} toggleAddFolder={toggleAddFolder}></DotPopoverMenu>
                 </Box>
             </Box>
         );
     }
 
-    const DotPopoverMenu = ({ids}:DotPopoverProps) => {
+    const DotPopoverMenu = ({treeId, toggleAddFolder}:DotPopoverProps) => {
         //const [open, setOpen] = useState(false);
         const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
 
         const handleClick = (event: React.MouseEvent<HTMLButtonElement>)=>{
-            console.log(event);
+            event.stopPropagation();
             setAnchorEl(event.currentTarget)
         }
-        const handleClose = ()=>{
+        const handleClose = (event: React.MouseEvent<HTMLLIElement>)=>{
+            event.stopPropagation();
             setAnchorEl(null)
+        }
+
+        const handleAddFolder = (event: React.MouseEvent<HTMLLIElement>) => {
+            toggleAddFolder();
+            handleClose(event)
         }
 
         const open = Boolean(anchorEl);
@@ -68,7 +89,7 @@ export const FolderTree = () => {
                     'aria-labelledby': 'basic-button',
                     }}
                 >
-                    <MenuItem onClick={handleClose}>
+                    <MenuItem onClick={handleAddFolder}>
                         <ListItemIcon>
                             <CreateNewFolder fontSize="small" />
                         </ListItemIcon>
@@ -141,21 +162,101 @@ export const FolderTree = () => {
         }
     ]
 
-    const TreeListing = ({folderData}: TreeListingProps)=>{
+    const TreeListing = ({folderData,folderHook, newflag, andChangeFlag}: TreeListingProps)=>{
+
         return (
             <>
-            {folderData.map((item)=>
-                <TreeItem key={item.id} nodeId={item.id.toString()} label={<BoxWithIcon label={item.name} />}>
-                    {item.childNodes.length ? <TreeListing folderData={item.childNodes}></TreeListing> : ""}
-                </TreeItem>
-            )}
+                {folderData.map((item)=>
+                    <TreeItemListComponent key={item.id} item={item} folderHook={folderHook} />
+                )}
+                {newflag ? <AddNewFolder toggleAddFolder={andChangeFlag}/> : ""}
             </>
         );
     }
 
-    const handleTreeViewEvent = (event:React.MouseEvent<HTMLButtonElement>)=>{
-        console.log("Tree event", event)
+    const TreeItemListComponent = ({item, folderHook}:{item:FolderMetaData, folderHook:CallableFunction}) => {
+        const {flag, changeFlag} = folderHook(false);
+        const anotherChangeFlag = useCallback(()=>{
+            changeFlag((val:boolean)=>!val)
+        }, [flag]);
+
+        return (
+            <TreeItem key={item.id} nodeId={item.id.toString()} label={<BoxWithIcon toggleAddFolder={anotherChangeFlag} id={item.id} label={item.name} />}>
+                {item.childNodes.length ? <TreeListing folderData={item.childNodes} folderHook={useToggleAddFolder} newflag={flag} andChangeFlag={anotherChangeFlag}></TreeListing> : ""}
+            </TreeItem>
+        )
     }
+
+    
+    const AddNewFolder = ({toggleAddFolder}:AddNewFolderProps) => {
+        const addFolderSubmit = () => {
+            
+        }
+    
+        const addFolderformik = useFormik({
+            initialValues: { folderName: "" },
+            validationSchema: AddFolderSchema,
+            onSubmit: addFolderSubmit,
+        });
+
+        const closeAddFolder = () => {
+            toggleAddFolder && toggleAddFolder();
+        }
+        return (
+            <TreeItem nodeId={`${Math.random()}`} label={
+                <Box
+                    id='add-project-form'
+                    component="form"
+                    noValidate
+                    onSubmit={addFolderformik.handleSubmit}
+                    sx={{ m: 0, ml: -2 }}
+                >
+                    <Grid container spacing={2}>
+                        <Grid item xs={8}>
+                            <TextField
+                                required
+                                variant='standard'
+                                fullWidth
+                                id="folderName"
+                                label="Enter folder name"
+                                name="folderName"
+                                autoComplete="folderName"
+                                value={addFolderformik.values.folderName}
+                                onChange={addFolderformik.handleChange}
+                                onBlur={addFolderformik.handleBlur}
+                                error={addFolderformik.touched.folderName && Boolean(addFolderformik.errors.folderName)}
+                                helperText={addFolderformik.touched.folderName && addFolderformik.errors.folderName}
+                            />
+                        </Grid>
+                        <Grid sx={{verticalAlign: "center", display: "flex"}} item xs={2}>
+                            <IconButton aria-describedby="add-folder-submit" type='submit' sx={{p: 0}}>
+                                <Check />
+                            </IconButton>
+                            <IconButton aria-describedby="add-folder-cancel" type='button' onClick={closeAddFolder} sx={{p: 0}}>
+                                <Close />
+                            </IconButton>
+                        </Grid>
+                    </Grid>
+                </Box>
+            }></TreeItem>
+        );
+    }
+
+    const handleTreeViewEvent = (event:React.MouseEvent<HTMLUListElement>)=>{
+        
+    }
+
+    const useToggleAddFolder = () => {
+        const [flag, setFlag] = useState(false);
+
+        return {
+            flag,
+            changeFlag: setFlag
+        }
+    }
+
+    const parentStpl = () => {};
+
    return (
       <>
         <Box component="h3" sx={{marginTop: 2}}>FOLDERS</Box>
@@ -165,7 +266,7 @@ export const FolderTree = () => {
             defaultExpandIcon={<ChevronRightIcon />}
             onClick={handleTreeViewEvent}
         >
-            <TreeListing folderData={tempData}></TreeListing>
+            <TreeListing folderData={tempData} folderHook={useToggleAddFolder} newflag={false} andChangeFlag={parentStpl}></TreeListing>
         </TreeView>
       </>
    );
